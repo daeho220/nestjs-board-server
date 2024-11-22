3af73a252a2f65113650263caa6210a7df63428c
daeho <daeho210@gmail.com>
2024-11-22 17:31:50 +0900

    Init: 댓글 기본 구조 생성 댓글 기본 구조 생성

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { PostsModule } from './posts/posts.module';
 import { AuthModule } from './auth/auth.module';
+import { CommentsModule } from './comments/comments.module';
 
 @Module({
-  imports: [UsersModule, PostsModule, AuthModule],
+  imports: [UsersModule, PostsModule, AuthModule, CommentsModule],
   controllers: [AppController],
   providers: [AppService],
 })
